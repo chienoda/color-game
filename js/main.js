05b9ef2859d77f5fda80393300347b9d77cf848c
Chie Noda <chienoda119@gmail.com>
2018-07-21 15:08:46 -0400

    Get random color to each squares

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-var colors = [
-'rgb(255, 0, 0)',
-'rgb(255, 255, 0)',
-'rgb(0, 255, 0)',
-'rgb(0, 255, 255)',
-'rgb(0, 0, 255)',
-'rgb(255, 0, 255)'
-]
+var colors = generateRandomColors(6);
+
+// [
+// 'rgb(255, 0, 0)',
+// 'rgb(255, 255, 0)',
+// 'rgb(0, 255, 0)',
+// 'rgb(0, 255, 255)',
+// 'rgb(0, 0, 255)',
+// 'rgb(255, 0, 255)'
+// ]
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
@@ -49,4 +51,27 @@ function pickColor() {
 	//pick random number
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = [];
+	//repeat num times
+	for(var i = 0; i < num; i++) {
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	//return that array
+	return arr;
+}
+
+//get random color
+function randomColor() {
+	//pick a 'red' from 0-255
+	var r = Math.floor(Math.random() * 256);
+	//pick a 'green' from 0-255
+	var g = Math.floor(Math.random() * 256);
+	//pick b 'blue' from 0-255
+	var b = Math.floor(Math.random() * 256);
+	return 'rgb('+ r + ', ' + g + ', ' + b + ')';
 }
